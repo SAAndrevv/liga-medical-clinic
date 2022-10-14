c337114e172253f19d364a59588b9e93805b2e6a
Andrey Sudomoykin <SAAndrew52@yandex.ru>
2022-10-15 00:55:47 +0300

    add rabbitMq

---
+++ core/src/main/java/liga/medical/medicalmonitoring/core/service/RabbitListenerService.java
@@ -0,0 +1,9 @@
+package liga.medical.medicalmonitoring.core.service;
+
+import liga.medical.medicalmonitoring.dto.rabbit.RabbitMessageDto;
+
+public interface RabbitListenerService {
+
+    void processCommonMonitoringQueue(RabbitMessageDto message);
+
+}
