41e30492bbb7ff1268b21e948337107d5f444416
Andrey Sudomoykin <SAAndrew52@yandex.ru>
2022-11-10 12:40:14 +0300

    little changes in LoggerAdvice

--- core/src/main/java/liga/medical/medicalmonitoring/core/service/RabbitListenerServiceImpl.java
+++ core/src/main/java/liga/medical/medicalmonitoring/core/service/RabbitListenerServiceImpl.java
@@ -1,6 +1,7 @@
 package liga.medical.medicalmonitoring.core.service;
 
 import liga.medical.commonmodule.dto.rabbit.RabbitMessageDto;
+import liga.medical.commonmodule.service.annotation.DBLog;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
@@ -18,6 +19,7 @@ public class RabbitListenerServiceImpl implements RabbitListenerService {
 
     @Override
     @RabbitListener(queues = "common_monitoring")
+    @DBLog
     public void processCommonMonitoringQueue(RabbitMessageDto message) {
         System.out.println("Get message: " + message);
         template.convertAndSend(message.getType(), message);
